51d646a71d494df2dad06eb473e6d9c41dadc9a4
diyar1990com21-blip <diyar1990com21@gmail.com>
2026-09-11 03:19:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -106,5 +106,6 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    if __name__ == '__main__':
+    # Hatalı PORI veya boşluk içeren değişkeni tamamen temizliyoruz
+    app.run(host='0.0.0.0', port=5000)
